7fa1cbebdc670e3a269c7006633cfb7778bcab2d
Joao Medeiros <jpsm1985@gmail.com>
2023-07-06 11:09:52 -0300

    Improves the steganography code

--- steganography/hide.py
+++ steganography/hide.py
@@ -1,10 +1,36 @@
+import os
 import cv2
 import sys
 
-def split_byte(byte):
+HEADER_SIZE = 8
+
+def pixel_from_size(size, width):
     """
     """
-    return byte >> 6 & 3, byte >> 4 & 3, byte >> 2 & 3, byte & 3
+    i = size // width
+    j = size % width
+    return i, j
+
+def next_pixel(i, j, width):
+    """
+    """
+    j = j + 1
+    if j == width:
+        i = i + 1
+        j = 0
+    return i, j
+
+def graft_byte_on_rgba(byte, pixel):
+    """
+    """
+    shift = 6
+    group = []
+    for c in range(len(pixel)):
+        group.append((byte >> shift) & 3)
+        shift = shift - 2
+    for c in range(len(pixel)):
+        pixel[c] = pixel[c] & 252 | group[c]
+    return pixel
 
 if __name__ == "__main__":
     """
@@ -12,33 +38,24 @@ if __name__ == "__main__":
     image = sys.argv[1]
     secret = sys.argv[2]
 
+    # open image
     array = cv2.imread(image, cv2.IMREAD_UNCHANGED)
     idata = open(secret, "rb")
 
+    # hide file on image array (two bits per channel in RGBA)
     height, width, channels = array.shape
-    i, j = 0, 8
+    i, j = 0, HEADER_SIZE
     byte = idata.read(1)
     while byte:
         byte = byte[0]
-        r, g, b, a = array[i][j]
-        slices = split_byte(byte)
-
-        r = r & 252 | slices[0]
-        g = g & 252 | slices[1]
-        b = b & 252 | slices[2]
-        a = a & 252 | slices[3]
-        array[i][j] = [r, g, b, a]
-
-        j = j + 1
-        if j == width:
-            i = i + 1
-            j = 0
-
+        array[i][j] = graft_byte_on_rgba(byte, array[i][j])
+        i, j = next_pixel(i, j, width)
         byte = idata.read(1)
 
-    size = (i * width + j - 8)
-    slices = [0] * 8
-    index = 7
+    # split the amount of written bytes in 8 bytes
+    size = (i * width + j - HEADER_SIZE)
+    slices = [0] * HEADER_SIZE
+    index = HEADER_SIZE - 1
     while size:
         slices[index] = size & 3
         slices[index] = slices[index] | ((size >> 2) & 3) << 2
@@ -48,23 +65,13 @@ if __name__ == "__main__":
         index = index - 1
         size = size >> 8
 
+    # update the first 8 pixels as the header
     i, j = 0, 0
-    byte = idata.read(1)
     index = 0
-    while index < 8:
-        r, g, b, a = array[i][j]
-        s = split_byte(slices[index])
-        r = r & 252 | s[0]
-        g = g & 252 | s[1]
-        b = b & 252 | s[2]
-        a = a & 252 | s[3]
-        array[i][j] = [r, g, b, a]
-
-        j = j + 1
-        if j == width:
-            i = i + 1
-            j = 0
-
+    while index < HEADER_SIZE:
+        array[i][j] = graft_byte_on_rgba(slices[index], array[i][j])
+        i, j = next_pixel(i, j, width)
         index = index + 1
 
-    cv2.imwrite('secret.png', array)
+    # write the image
+    cv2.imwrite(os.path.basename(secret) + '.png', array)
--- steganography/show.py
+++ steganography/show.py
@@ -1,21 +1,60 @@
 import cv2
 import sys
+import numpy
 
-def merge_byte(r, g, b, a):
+HEADER_SIZE = 8
+
+def pixel_from_size(size, width):
     """
     """
-    return ((r & 3) << 6) | ((g & 3) << 4) | ((b & 3) << 2) | (a & 3)
+    i = size // width
+    j = size % width
+    return i, j
+
+def next_pixel(i, j, width):
+    """
+    """
+    j = j + 1
+    if j == width:
+        i = i + 1
+        j = 0
+    return i, j
+
+def merge_byte_from_pixel(pixel):
+    """
+    """
+    r, g, b, a = pixel
+    byte = ((r & 3) << 6) | ((g & 3) << 4) | ((b & 3) << 2) | (a & 3)
+    return byte
 
 if __name__ == "__main__":
     """
     """
     image = sys.argv[1]
 
+    # open the image
     array = cv2.imread(image, cv2.IMREAD_UNCHANGED)
-
     height, width, channels = array.shape
+
+    # extract the amount of bytes from the first 8 pixels
     i, j = 0, 0
-    for i in range(height):
-        for j in range(width):
-            r, g, b, a = array[i][j]
-            print(bin(merge_byte(r, g, b, a)), merge_byte(r, g, b, a))
+    index = 0
+    slices = []
+    while index < HEADER_SIZE:
+        slices.append(merge_byte_from_pixel(array[i][j]))
+        i, j = next_pixel(i, j, width)
+        index = index + 1
+
+    # compute the size from byte slices
+    size = 0
+    for byte in slices:
+        size = (size << 8) | byte
+
+    # output the hidden bytes
+    i, j = pixel_from_size(HEADER_SIZE, width)
+    index = 0
+    while index < size:
+        byte = numpy.int8(merge_byte_from_pixel(array[i][j]))
+        sys.stdout.buffer.write(byte)
+        i, j = next_pixel(i, j, width)
+        index = index + 1
